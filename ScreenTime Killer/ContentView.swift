9f4feb28b97a780bda3d6eae87a2165e500d05f6
Ido Nov <idonov8@gmail.com>
2024-08-20 17:41:14 +0700

    Initial Commit

---
+++ ScreenTime Killer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenTime Killer
+//
+//  Created by Ido Nov on 20/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenTime Killer/ScreenTime_KillerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTime_KillerApp.swift
+//  ScreenTime Killer
+//
+//  Created by Ido Nov on 20/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTime_KillerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
